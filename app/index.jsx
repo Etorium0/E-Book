6e4901551a4d250c4ffa7783cc29fc8a293fb6b8
TTMTN <22520936@gm.uit.edu.vn>
2025-01-10 02:49:18 +0700

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,33 @@
-import { Button, Text, View } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
-import ScreenWrapper from '@/components/ScreenWrapper';
-import 'react-native-gesture-handler'
+import { StyleSheet, ImageBackground, Pressable } from 'react-native';
+import React from 'react';
+import { useRouter } from 'expo-router';
+import 'react-native-gesture-handler';
 import 'react-native-reanimated';
+
+const backgroundImage = require('../assets/images/LoadingScreen.png'); // Replace with your image path
 
 const index = () => {
   const router = useRouter();
-  console.log(router);
-  return (
-    <ScreenWrapper>
-      <Text>index</Text>
-      <Button title='welcome' onPress={()=>router.push('HomeScreens')} />
-    </ScreenWrapper>
-  )
-}
 
-export default index
+  const handlePress = () => {
+    router.push('Login'); // Navigate to the 'signUp' screen
+  };
+
+  return (
+    <Pressable style={styles.container} onPress={handlePress}>
+      <ImageBackground source={backgroundImage} style={styles.background} />
+    </Pressable>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // Ensures the image covers the entire screen
+  },
+});
